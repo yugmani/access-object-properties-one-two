2e6d17dfe3789971a42b6ac73ccc28fcbd00bc88
yugmani <yugmani@yahoo.com>
2021-05-13 13:33:35 -0700

    A function to add property to each object in array without mutating

--- script.js
+++ script.js
@@ -153,8 +153,42 @@ function swapKeyAndValues(obj) {
 // Expected { city: 'Berlin', country: 'Germany' }
 
 // ***********************************************************
-// 1. Accessing object properties two
+// 6. Add property to each object in array
 // ***********************************************************
+
+// Write a function that takes an array of objects and a string as arguments
+// Add a property with key 'continent' and value equal to the string to each of the objects
+// Return the new array of objects
+// Tipp: try not to mutate the original array
+
+//Solution
+function addPropertyToArrayOfObjects(arr, string) {
+  return arr.map(item => {
+    return { ...item, continent: string };
+  });
+}
+
+// Test Cases:
+console.log(
+  addPropertyToArrayOfObjects(
+    [
+      { city: 'Tokyo', country: 'Japan' },
+      { city: 'Bangkok', country: 'Thailand' }
+    ],
+    'Asia'
+  )
+);
+// Expected [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
+console.log(
+  addPropertyToArrayOfObjects(
+    [
+      { city: 'Stockholm', country: 'Sweden' },
+      { city: 'Paris', country: 'France' }
+    ],
+    'Europe'
+  )
+);
+// Expected [{ city: 'Stockholm', country: 'Sweden', continent: 'Europe' }, { city: 'Paris', country: 'France', continent: 'Europe' }]
 
 // ***********************************************************
 // 1. Accessing object properties two
