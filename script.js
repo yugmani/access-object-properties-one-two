ea28645d576a5896b7834eb52c026d52e6855c2f
yugmani <yugmani@yahoo.com>
2021-05-12 22:55:36 -0700

    access property of key given as a string

--- script.js
+++ script.js
@@ -36,8 +36,29 @@ function accessObjProperties(obj) {
 // console.log(accessObjProperties({ 'prop-2': 'two', prop: 'test' })); // Expected 'two'
 
 // ***********************************************************
-// 1. Accessing object properties two
+// 3. Accessing object properties three
 // ***********************************************************
+
+// Write a function that takes a string and an object with two properties as arguments
+// It should return the value of the property with key equal to the value of the string
+
+//My Solution
+function accessKeyString({ continent, country }, string) {
+  return string === 'continent' ? continent : country;
+}
+
+// // Author's Solution
+// function accessKeyString(obj, key) {
+//   return obj[key];
+// }
+
+// Test Cases:
+console.log(
+  accessKeyString({ continent: 'Asia', country: 'Japan' }, 'continent')
+); //Expected 'Asia'
+console.log(
+  accessKeyString({ country: 'Sweden', continent: 'Europe' }, 'country')
+); // Expected 'Sweden'
 
 // ***********************************************************
 // 1. Accessing object properties two
