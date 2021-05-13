73fca3db5748e2220b5695afa922ed25d5185823
yugmani <yugmani@yahoo.com>
2021-05-12 22:46:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-objects-challenge
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-objects-challenge)
---
+++ script.js
@@ -0,0 +1,56 @@
+// ***********************************************************
+// 1. Accessing object properties One
+// ***********************************************************
+
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
+
+//My Code
+function accessObjKey({ continent, country }) {
+  return country;
+}
+
+// //Author's Code
+// function myFunction(obj) {
+//   return obj.country;
+// }
+
+// Test Cases:
+console.log(accessObjKey({ continent: 'Asia', country: 'Japan' })); //Expected 'Japan'
+console.log(accessObjKey({ country: 'Sweden', continent: 'Europe' })); // Expected 'Sweden'
+
+// ***********************************************************
+// 2. Accessing object properties two
+// ***********************************************************
+
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key 'prop-2'
+// Tipp: you might want to use the square brackets property accessor
+
+function accessObjProperties(obj) {
+  return obj['prop-2'];
+}
+
+// Test Cases:
+// console.log(accessObjProperties({ one: 1, 'prop-2': 2 })); // Expected 2
+// console.log(accessObjProperties({ 'prop-2': 'two', prop: 'test' })); // Expected 'two'
+
+// ***********************************************************
+// 1. Accessing object properties two
+// ***********************************************************
+
+// ***********************************************************
+// 1. Accessing object properties two
+// ***********************************************************
+
+// ***********************************************************
+// 1. Accessing object properties two
+// ***********************************************************
+
+// ***********************************************************
+// 1. Accessing object properties two
+// ***********************************************************
+
+// ***********************************************************
+// 1. Accessing object properties two
+// ***********************************************************
